bd0fb2f1b7d742775f3fa79ecee748b29bc5ee5d
Anita <writeanita.n@gmail.com>
2024-06-29 18:33:12 +0100

    Added collision detection and end game functionality

--- script.js
+++ script.js
@@ -16,19 +16,19 @@ function moveLine() {
         }
         item.y += player.speed;
         item.style.top = item.y + "px";
-    })
+    });
 }
 
-function isCollide(a,b) {
+function isCollide(a, b) {
     let aRect = a.getBoundingClientRect();
-    let bReact = b.getBoundingClientRect();
+    let bRect = b.getBoundingClientRect();
 
     return !(
-        (aRect.bottom < bReact.top) ||
-        (aRect.top > bReact.bottom) ||
-        (aRect.right < bReact.left) ||
-        (aRect.left > aRect.right)
-    )
+        (aRect.bottom < bRect.top) ||
+        (aRect.top > bRect.bottom) ||
+        (aRect.right < bRect.left) ||
+        (aRect.left > bRect.right)
+    );
 }
 
 function moveEnemy(car) {
@@ -40,11 +40,11 @@ function moveEnemy(car) {
         }
         if(item.y >= 1500) {
             item.y = -600;
-            item.style.left = Math.floor(Math.random() * 150) + "px";
+            item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + "px";
         }
         item.y += player.speed;
         item.style.top = item.y + "px";
-    })
+    });
 }
 
 function playGame() {
@@ -88,13 +88,12 @@ function pressOff(e) {
 
 function endGame() {
     player.start = false;
-    score.innerHTML = "Game Over<br> Score was" + player.score;
+    score.innerHTML = "Game Over<br> Score was " + player.score;
     startScreen.classList.remove("hide");
 }
 
 function start() {
     startScreen.classList.add("hide");
-    // gameArea.classList.remove("hide");
     gameArea.innerHTML = "";
     player.start = true;
     player.score = 0;
@@ -117,10 +116,10 @@ function start() {
     for(let x = 0; x < 3; x++) {
         let enemy = document.createElement("div");
         enemy.classList.add("enemy");
-        enemy.innerHTML = (x+1);
-        enemy.y = ((x+1)*600)*-1;
+        enemy.innerHTML = (x + 1);
+        enemy.y = ((x + 1) * 600) * -1;
         enemy.style.top = enemy.y + "px";
-        enemy.style.left = Math.floor(Math.random()*150) + "px";
+        enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + "px";
         enemy.style.backgroundColor = randomColor();
         gameArea.appendChild(enemy);
     }
@@ -128,8 +127,8 @@ function start() {
 
 function randomColor() {
     function c() {
-        let Hex = Math.floor(Math.random()*256).toString(16);
-        return("0" + String(Hex)).substring(-2);
+        let hex = Math.floor(Math.random() * 256).toString(16);
+        return ("0" + String(hex)).substr(-2);
     }
-    return "#"+c()+c()+c();
+    return "#" + c() + c() + c();
 }
